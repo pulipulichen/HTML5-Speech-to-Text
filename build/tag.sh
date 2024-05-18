3bf0313b0511204889334d3c25e2c254d8628efa
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2024-05-18 18:24:12 +0800

    西元2024年05月18日 (週六) 18時24分12秒 CST

--- build/tag.sh
+++ build/tag.sh
@@ -9,7 +9,7 @@ new_version=$(date '+%Y%m%d.%H%M%S')
 git tag $new_version
 git push origin $new_version
 
-# =================================================================
+# # =================================================================
 
 git add .
 git commit -m "${new_version}"
@@ -17,4 +17,17 @@ git push --force-with-lease
 
 # =================================================================
 
+GITHUB_HOMEPAGE=`jq -r '.homepage' package.json`
+#echo $GITHUB_HOMEPAGE
+
+GITHUB_USER=`echo $GITHUB_HOMEPAGE | awk -F'/' '{print $4}'`
+
+GITHUB_REPO=`jq -r '.name' package.json`
+#echo $GITHUB_REPO
+
+echo "================================================"
 echo "Integrate GitHub with Zenodo: https://rb.gy/ql60qi"
+echo "GitHub Homepage:  ${GITHUB_HOMEPAGE}"
+echo "Zenono GitHub Setting:  https://zenodo.org/account/settings/github/"
+echo "Zenono Repo:  https://zenodo.org/account/settings/github/repository/${GITHUB_USER}/${GITHUB_REPO}"
+echo "================================================"
